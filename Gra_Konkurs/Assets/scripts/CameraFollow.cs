2518ff204829b31ed43a27bb5218f0ab9a88982e
JakubM2 <jakubmarcinmiros@gmail.com>
2017-04-18 19:58:54 +0200

    Czwarty tydzień cd

---
+++ Gra_Konkurs/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float m_speed = 0.1f;
+    Camera mycam;
+
+    void Start()
+    {
+        mycam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+
+        if (target == null)
+            return;
+
+        mycam.orthographicSize = (Screen.height / 100f);
+
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, m_speed) + new Vector3(0, 0, -10);
+        }
+    }
+}
+
---
+++ Gra_Konkurs/Assets/scripts/MenuScript.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour
+{
+    //add intro&logo to game
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f;
+
+    public RectTransform menuContainer;
+    public Transform NewGame_Panel;
+    public Transform Options_Panel;
+
+    private Vector3 desieredMenuPosition;
+    
+    private void Start()
+    {
+        //fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        //fadeGroup.alpha = 1;
+
+        initOptions();
+
+        //add buttons on-click event to levels
+        initNewGame();
+    }
+
+    private void Update()
+    {
+        //fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+
+        //menu navigation
+        menuContainer.anchoredPosition3D = Vector3.Lerp(menuContainer.anchoredPosition3D, desieredMenuPosition, 0.1f);
+    }
+
+    private void initNewGame()
+    {
+        if (NewGame_Panel == null)
+        {
+            Debug.Log("I dont see new Game panel!");
+        }
+        int i = 0;
+        foreach (Transform t in NewGame_Panel)
+        {
+            int currentIndex = i;
+            Button b = t.GetComponent<Button>();//add few another... for example textbox
+            //Text tx = t.GetComponent<Text>();
+            //b.onClick.AddListener(() => newGamePanel(currentIndex));
+
+            i++;
+        }
+    }
+
+    private void initOptions()
+    {
+        if(Options_Panel == null)
+        {
+            Debug.Log("I dont see options panel!");
+        }
+        int i = 0;
+        foreach(Transform t in Options_Panel)
+        {
+            int currentIndex = i;
+            Button b = t.GetComponent<Button>();//add few another... for example textbox
+            //Text tx = t.GetComponent<Text>();
+            //b.onClick.AddListener(() => optionPanel(currentIndex));
+
+            i++;
+        }
+        //Reset index
+        i = 0;
+        //optional we can add another panel --- 12:44 YT
+    }
+
+    private void NavigateTo(int menuIndex)
+    {
+        switch (menuIndex)
+        {
+            default:
+            case 0:
+                desieredMenuPosition = Vector3.zero;
+                break;
+            case 1:
+                desieredMenuPosition = Vector3.right * Screen.width;
+                break;
+            case 2:
+                desieredMenuPosition = Vector3.left * Screen.width;
+                break;
+            case 3:
+                desieredMenuPosition = Vector3.down * Screen.height;
+                break;
+            case 4:
+                desieredMenuPosition = Vector3.up * Screen.height;
+                break;
+        }
+    }
+
+    //button
+    public void newGame()
+    {
+        NavigateTo(1);
+        Debug.Log("NewGame");
+    }
+    public void continueGame()
+    {
+        NavigateTo(3);
+        Debug.Log("continue");
+    }
+    public void options()
+    {
+        NavigateTo(2);
+        Debug.Log("options");
+    }
+    public void info()
+    {
+        NavigateTo(4);
+        Debug.Log("info");
+    }
+    public void exit()
+    {
+        Debug.Log("exit");
+        Application.Quit();
+    }
+    public void backToMenu()
+    {
+        NavigateTo(0);
+        Debug.Log("back to menu");
+    }
+    public void startGame()
+    {
+        //load game scene
+        Application.LoadLevel(1);
+    }
+
+    //
+    private void optionPanel(int currentIndex)
+    {
+        Debug.Log("Selecting options: " + currentIndex);
+    }
+    private void newGamePanel(int currentIndex)
+    {
+        Debug.Log("Selecting Levels: " + currentIndex);
+    }
+}
