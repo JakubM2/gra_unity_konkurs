d05dd968e0bfa2d19ac628de882c129837f0009b
JakubM2 <jakubmarcinmiros@gmail.com>
2017-04-23 21:10:56 +0200

    Piąty tydzień Pracy

---
+++ Gra_Konkurs/Assets/scripts/PauseMenu.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    //
+    public GameObject PauseUI;
+
+    private bool paused = false;
+
+    void Start()
+    {
+        PauseUI.SetActive(false);
+    }
+    void Update()
+    {
+        if(Input.GetButtonDown("Pause"))
+        {
+            paused = !paused;
+        }
+        if(paused)
+        {
+            PauseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+        if(!paused)
+        {
+            PauseUI.SetActive(false);
+            Time.timeScale = 1;
+        }
+    }
+
+
+    //button
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Restart()
+    {
+        //Options
+        Application.LoadLevel(Application.loadedLevel);
+    }
+    public void SaveGame()
+    {
+        //add save game script
+    }
+    public void MainMenu()
+    {
+        Application.LoadLevel(0);
+    }
+    public void Resume()
+    {
+        paused = false;
+    }
+}
